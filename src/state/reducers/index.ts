cb51f6beb548db569848e3a9be9b31a4b6a0cbe4
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-06 18:34:53 +0200

    Using action creators in react component

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import bankReducer from "./bankReducer";
 
-export default combineReducers({
+const reducers = combineReducers({
     bank: bankReducer,
 });
+
+export default reducers;
+
+export type State = ReturnType<typeof reducers>;
