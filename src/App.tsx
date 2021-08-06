2acfe552a5fbb1242c129d5cbd10c1b0cb3857b8
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-06 18:57:33 +0200

    Styling the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators, State } from "./state";
+import "./style/style.css";
 
 function App() {
     const dispatch = useDispatch();
@@ -13,11 +14,25 @@ function App() {
     const amount = useSelector((state: State) => state.bank);
 
     return (
-        <div className="App">
-            <h1>{amount}</h1>
-            <button onClick={() => depositMoney(1000)}>Deposit</button>
-            <button onClick={() => withdrawMoney(500)}>Withdraw</button>
-            <button onClick={() => bankrupt()}>Bankrupt</button>
+        <div className="container">
+            <div className="app">
+                <h1 className="app__header">{amount}</h1>
+                <button
+                    className="app__button"
+                    onClick={() => depositMoney(1000)}
+                >
+                    Deposit
+                </button>
+                <button
+                    className="app__button"
+                    onClick={() => withdrawMoney(500)}
+                >
+                    Withdraw
+                </button>
+                <button className="app__button" onClick={() => bankrupt()}>
+                    Bankrupt
+                </button>
+            </div>
         </div>
     );
 }
